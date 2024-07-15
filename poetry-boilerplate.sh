310bddb340d48337eb6d553fe66ac8ab0569c41f
R Raj <rajbansh4@gmail.com>
2024-07-14 23:41:13 -0700

    add vanilla boilerplate script

--- poetry-boilerplate.sh
+++ poetry-boilerplate.sh
@@ -7,34 +7,15 @@ read -p "Enter the python project name:" PROJECT_NAME
 # Replace hyphens with underscores
 PROJECT_NAME="${PROJECT_NAME//-/_}"
 
-# Path to the virtual environment
-VENV_PATH="venv"
-
-# Create a folder scaffolding
-mkdir -p $PROJECT_NAME/$PROJECT_NAME
-mkdir -p $PROJECT_NAME/tests
-touch $PROJECT_NAME/$PROJECT_NAME/__init__.py
-touch $PROJECT_NAME/tests/__init__.py
-
+# Create a poetry project
+poetry new $PROJECT_NAME
 cd $PROJECT_NAME
-python3 -m ${VENV_PATH} ${VENV_PATH}
-source ${VENV_PATH}/bin/activate
 
-# Install python dependencies
-python3 -m pip install flake8 black isort pytest pytest-cov mypy pytest-socket pytest-mock pre-commit ruff build
-
-cat << EOM > pyproject.toml
-[project]
-name = "starter"
-version = "0.1.0"
-description = ""
-authors = [{name="R Raj", email="rajbansh4@gmail.com"}]
-readme = "README.md"
-EOM
-
+# Install poetry dependencies
+poetry add flake8 black isort pytest pytest-cov mypy pytest-socket pytest-mock pre-commit
 
 # Update python configuration 
-cat << EOM > setup.cfg
+cat << EOM >> setup.cfg
 [coverage:run]
 # Include only these source files for coverage measurement
 source = ${PROJECT_NAME}
@@ -70,7 +51,7 @@ directory = coverage_html_report
 [flake8]
 max-line-length = 160
 ignore = E226,E302,E41
-exclude = .git, __pycache__, build, dist, ${VENV_PATH}
+exclude = .git, __pycache__, build, dist
 extend-ignore = E203, W503
 max-complexity=10
 
@@ -78,7 +59,7 @@ max-complexity=10
 addopts = --disable-socket
 EOM
 
-cat << EOM > .pre-commit-config.yaml
+cat << EOM >> .pre-commit-config.yaml
 repos:
   - repo: https://github.com/pre-commit/pre-commit-hooks
     rev: v3.2.0
@@ -98,61 +79,53 @@ repos:
 EOM
 
 # Generate Makefile
-cat << EOM > Makefile
+cat << EOM >> Makefile
 # Variables
 PYTHON = python3
 PIP = pip
+POETRY = poetry
 PROJECT = $PROJECT_NAME
-
-# Path to the virtual environment
-VENV_PATH = $VENV_PATH
 
 # Default target
 all: install
 
 # Rules
-add: ## Git add
-	. \$(VENV_PATH)/bin/activate && pip freeze > requirements.txt
-
 install: ## Install project dependencies
-	. venv/bin/activate && pip install -r requirements.txt
+	\$(POETRY) install
 
 test: ## Run tests using pytest
-	. \$(VENV_PATH)/bin/activate && pytest
+	\$(POETRY) run pytest
 
 lint: ## Lint the code using flake8
-	. \$(VENV_PATH)/bin/activate && flake8 .
+	\$(POETRY) run flake8 .
 
 format: ## Format the code using black
-	. \$(VENV_PATH)/bin/activate && black .
+	\$(POETRY) run black .
 
 sort: ## Format the code using black
-	. \$(VENV_PATH)/bin/activate && isort .
+	\$(POETRY) run isort .
 
 mypy: ## Format the code using black
-	. \$(VENV_PATH)/bin/activate && mypy .
-
-ruff: ## Ruff check
-	. \$(VENV_PATH)/bin/activate && ruff check
-
-fix: ## Ruff fix
-	. \$(VENV_PATH)/bin/activate && ruff check --fix
+	\$(POETRY) run mypy .
 
 run: ## Run main file
-	. \$(VENV_PATH)/bin/activate && \$(PYTHON) \$(PROJECT)/main.py
+	\$(POETRY) run \$(PYTHON) \$(PROJECT)/main.py
 
-check: format lint sort mypy test add ## Run both linting and tests
+check: format lint sort mypy test ## Run both linting and tests
 
 clean: ## Clean up cache files and directories
 	find . -type d -name "__pycache__" -exec rm -r {} +
 	find . -type d -name "*.egg-info" -exec rm -r {} +
 	rm -rf .mypy_cache .pytest_cache .coverage
 
-build: ## Build the project
-	. \$(VENV_PATH)/bin/activate && \$(PYTHON) -m build
+build: ## Build the project using Poetry
+	\$(POETRY) build
+
+publish: ## Publish the project to PyPI using Poetry
+	\$(POETRY) publish --build
 
 coverage: ## Run tests and generate coverage report
-	. \$(VENV_PATH)/bin/activate && pytest --cov=. --cov-report=term-missing --cov-report=html
+	\$(POETRY) run pytest --cov=. --cov-report=term-missing --cov-report=html
 
 help: ## Show this help message
 	@echo "Usage: make [target]"
@@ -161,18 +134,18 @@ help: ## Show this help message
 	@awk 'BEGIN {FS = ":.*##"; printf "  %-15s %s\n", "Target", "Description"; printf "  %-15s %s\n", "------", "-----------"} /^[a-zA-Z_-]+:.*##/ { printf "  %-15s %s\n", \$\$1, \$\$2 }' \$(MAKEFILE_LIST)
 
 # Phony targets
-.PHONY: all install test lint format check clean build publish help run mypy sort coverage ruff fix
+.PHONY: all install test lint format check clean build publish help run mypy sort coverage
 EOM
 
 # Intialize empty git
 git init
 
 # Install pre-commit hooks and run
-. $VENV_PATH/bin/activate && pre-commit install
-. $VENV_PATH/bin/activate && pre-commit run
+poetry run pre-commit install
+poetry run pre-commit run
 
 # Generate sample python code and unit test
-cat << EOM > $PROJECT_NAME/main.py
+cat << EOM >> $PROJECT_NAME/main.py
 def hello():
     return "hello"
 
@@ -181,7 +154,7 @@ if __name__ == "__main__":
     print(hello())
 EOM
 
-cat << EOM > tests/test_main.py
+cat << EOM >> tests/test_main.py
 from $PROJECT_NAME.main import hello
 
 def test_hello():
@@ -189,8 +162,6 @@ def test_hello():
 EOM
 
 touch $PROJECT_NAME/py.typed
-
-pip freeze > requirements.txt
 
 echo "Setup git with 'git remote add origin git@github.com:User/UserRepo.git'"
 echo "git push -u origin main"
